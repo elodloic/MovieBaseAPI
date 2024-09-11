5ab2f3e998abab0c87542fce5a517c211c492bc4
elodloic <95284344+elodloic@users.noreply.github.com>
2024-09-12 00:18:56 +0200

    Update index.js
    Added testing domain

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS setup
 const cors = require("cors");
-let allowedOrigins = ["*"];
+let allowedOrigins = ["http://localhost:1234"];
 app.use(
   cors({
     origin: (origin, callback) => {
