ec4c32e7db7d7974a139cb69da375df8b78fe3db
elodloic <95284344+elodloic@users.noreply.github.com>
2024-11-07 14:42:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ let allowedOrigins = [
   "http://localhost:1234",
   "http://localhost:4200",
   "https://projectmoviebase.netlify.app",
+  "https://elodloic.github.io/MovieBase-Angular-client",
 ];
 app.use(
   cors({
